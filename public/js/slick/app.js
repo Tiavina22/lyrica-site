426ca86d4c8d562e59b1ef7c1eb4c7ee47f318f6
Tiavina22 <tiavinaramilison22.com>
2024-09-03 17:53:15 +0300

    First commit for the website lyrica

---
+++ public/js/slick/app.js
@@ -0,0 +1,22 @@
+$('.screenshoot').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 900,
+    responsive: [
+        {
+            breakpoint: 1070, 
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 650, 
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+  });
