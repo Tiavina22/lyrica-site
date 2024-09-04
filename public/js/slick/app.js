6c90bba9052d86ae10352561af21aade21f1cd69
Tiavina22 <tiavinaramilison22.com>
2024-09-04 16:44:02 +0300

    Gestion error

--- public/js/slick/app.js
+++ public/js/slick/app.js
@@ -20,24 +20,39 @@ $('.screenshoot').slick({
         }
     ]
 });
-
 document.addEventListener('DOMContentLoaded', () => {
     const downloadButton = document.querySelector('#downloadButton');
     const downloadCountElement = document.querySelector('#downloadCount');
 
+    // Fonction pour obtenir le nombre de téléchargements
     const getDownloadCount = async () => {
-        const response = await fetch('/api/downloads');
-        const data = await response.json();
-        downloadCountElement.textContent = `Nombre de téléchargements : ${data.count}`;
+        try {
+            const response = await fetch('/api/downloads');
+            if (!response.ok) throw new Error('Erreur de réseau');
+            const data = await response.json();
+            downloadCountElement.textContent = `Nombre de téléchargements : ${data.count}`;
+        } catch (error) {
+            console.error('Erreur lors de la récupération du nombre de téléchargements :', error);
+            downloadCountElement.textContent = 'Erreur lors de la récupération du nombre de téléchargements';
+        }
     };
 
+    // Fonction pour incrémenter le nombre de téléchargements
     const incrementDownloadCount = async () => {
-        await fetch('/api/downloads', { method: 'POST' });
-        getDownloadCount();
+        try {
+            const response = await fetch('/api/downloads', { method: 'POST' });
+            if (!response.ok) throw new Error('Erreur de réseau');
+            const data = await response.json();
+            downloadCountElement.textContent = `Nombre de téléchargements : ${data.count}`;
+        } catch (error) {
+            console.error('Erreur lors de l\'incrémentation du nombre de téléchargements :', error);
+        }
     };
 
+    // Mettre à jour le nombre de téléchargements au chargement de la page
     getDownloadCount();
 
+    // Événement de clic sur le bouton de téléchargement
     downloadButton.addEventListener('click', () => {
         incrementDownloadCount();
     });
