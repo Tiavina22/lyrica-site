f229be352ef35fa7a4520ff91f6dc30583f9ff2a
Tiavina22 <tiavinaramilison22.com>
2024-09-04 16:58:15 +0300

    Fixed also

--- public/js/slick/app.js
+++ public/js/slick/app.js
@@ -20,37 +20,3 @@ $('.screenshoot').slick({
         }
     ]
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const downloadButton = document.querySelector('#downloadButton');
-    const downloadCountElement = document.querySelector('#downloadCount');
-
-    const getDownloadCount = async () => {
-        try {
-            const response = await fetch('/api/downloads');
-            if (!response.ok) throw new Error('Erreur de réseau');
-            const data = await response.json();
-            downloadCountElement.textContent = `Nombre de téléchargements : ${data.count}`;
-        } catch (error) {
-            console.error('Erreur lors de la récupération du nombre de téléchargements :', error);
-            downloadCountElement.textContent = 'Erreur lors de la récupération du nombre de téléchargements';
-        }
-    };
-
-    const incrementDownloadCount = async () => {
-        try {
-            const response = await fetch('/api/downloads', { method: 'POST' });
-            if (!response.ok) throw new Error('Erreur de réseau');
-            const data = await response.json();
-            downloadCountElement.textContent = `Nombre de téléchargements : ${data.count}`;
-        } catch (error) {
-            console.error('Erreur lors de l\'incrémentation du nombre de téléchargements :', error);
-        }
-    };
-
-    getDownloadCount();
-
-    downloadButton.addEventListener('click', () => {
-        incrementDownloadCount();
-    });
-});
